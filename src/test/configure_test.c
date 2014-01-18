7bba111a09f3f1f86760eae38d6b087e8917d41f
Josh DeWitt <chessami92@gmail.com>
2014-01-18 14:38:04 -0600

    Added more tests for the configuration code.

--- src/test/configure_test.c
+++ src/test/configure_test.c
@@ -2,9 +2,13 @@
 #include "configure.c"
 #include "CuTest.h"
 
-static void configureTest( CuTest* tc );
+static void configureString( CuTest *tc, char *properties, int success );
+static void configureMmTest( CuTest *tc );
+static void configureInchTest( CuTest *tc );
+static void configureCommentsTest( CuTest *tc );
+static void configureBadPropertyTest( CuTest *tc );
 
-static void configureTest( CuTest* tc ) {
+static void configureString( CuTest *tc, char *properties, int success ) {
     int fd[2];
     FILE *readFile, *writeFile;
 
@@ -14,19 +18,39 @@ static void configureTest( CuTest* tc ) {
     readFile = fdopen( fd[0], "r" );
     CuAssert( tc, "Didn't open read file.", readFile != NULL );
 
-    fprintf( writeFile, "step.ratio=1.03452\n" );
+    fprintf( writeFile, properties );
     fclose( writeFile );
 
-    CuAssert( tc, "Configure failed.", configure( readFile ) );
-    CuAssert( tc, "Didn't set the stepRatio correctly.", stepRatio == 1.03452 );
+    CuAssert( tc, "Configure failed.", configure( readFile ) == success );
 
     fclose( readFile );
+}
+
+static void configureMmTest( CuTest *tc ) {
+    configureString( tc, "step.ratio=1.03452\n", 1 );
+    CuAssert( tc, "Didn't set the stepRatio correctly.", stepRatio == 1.03452 );
+}
+
+static void configureInchTest( CuTest *tc ) {
+    configureString( tc, "step.ratio=1.03452\n", 1 );
+    CuAssert( tc, "Didn't set the stepRatio correctly.", stepRatio == 1.03452 );
+}
+
+static void configureCommentsTest( CuTest *tc ) {
+    configureString( tc, "#Some comment\n#Another Comment\n", 1 );
+}
+
+static void configureBadPropertyTest( CuTest *tc ) {
+    configureString( tc, "unknown.property\n", 0 );
 }
 
 CuSuite* CuGetSuite( void ) {
     CuSuite* suite = CuSuiteNew();
 
-    SUITE_ADD_TEST( suite, configureTest );
+    SUITE_ADD_TEST( suite, configureMmTest );
+    SUITE_ADD_TEST( suite, configureInchTest );
+    SUITE_ADD_TEST( suite, configureCommentsTest );
+    SUITE_ADD_TEST( suite, configureBadPropertyTest );
 
     return suite;
 }
