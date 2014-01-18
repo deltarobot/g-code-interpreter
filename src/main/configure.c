e41a384eb43e926deca8a3219d88f263a57a701f
Josh DeWitt <chessami92@gmail.com>
2014-01-18 14:29:00 -0600

    Generalized the property gathering code.

--- src/main/configure.c
+++ src/main/configure.c
@@ -3,28 +3,21 @@
 #include <string.h>
 #include "configure.h"
 
+enum ReturnType {
+    Double = 0
+};
+
+union ReturnValue {
+    double doubleReturn;
+};
+
+typedef enum ReturnType ReturnType;
+typedef union ReturnValue ReturnValue;
+
 double stepRatio;
 
 static int processLine( char *line );
-
-static int processLine( char *line ) {
-    const char *stepRatioString = "step.ratio=";
-    const int stepRatioLength = strlen( stepRatioString );
-
-    if( line[0] == '#' ) {
-
-    } else if( strncmp( stepRatioString, line, stepRatioLength ) == 0 ) {
-        stepRatio = strtod( ( line + stepRatioLength ), NULL );
-        if( stepRatio <= 0 ) {
-            fprintf( stderr, "step.ratio must be strictly positive.\n" );
-            return 0;
-        }
-    } else {
-        fprintf( stderr, "Unknown configuration line: %s", line );
-        return 0;
-    }
-    return 1;
-}
+static int readProperty( char *propertyName, char *line, ReturnType returnType, ReturnValue *returnValue );
 
 int configure( FILE *file ) {
     char *line = NULL;
@@ -37,4 +30,39 @@ int configure( FILE *file ) {
     }
 
     return 1;
+}
+
+static int processLine( char *line ) {
+    ReturnValue returnValue;
+
+    if( line[0] == '#' ) {
+
+    } else if( readProperty( "step.ratio=", line, Double, &returnValue ) ) {
+        if( returnValue.doubleReturn <= 0 ) {
+            fprintf( stderr, "step.ratio must be strictly positive.\n" );
+            return 0;
+        } else {
+            stepRatio = returnValue.doubleReturn;
+        }
+    } else {
+        fprintf( stderr, "Unknown configuration line: %s", line );
+        return 0;
+    }
+    return 1;
+}
+
+static int readProperty( char *propertyName, char *line, ReturnType returnType, ReturnValue *returnValue ) {
+    int propertyLength = strlen( propertyName );
+
+    if( strncmp( propertyName, line, propertyLength ) == 0 ) {
+        switch( returnType ) {
+            case Double:
+                returnValue->doubleReturn = strtod( ( line + propertyLength ), NULL );
+                return 1;
+            default:
+                return 0;
+        }
+    } else {
+        return 0;
+    }
 }
