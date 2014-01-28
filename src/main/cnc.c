57348fcf69644e4dd7af5f4cc8d7d04292882f8b
Josh DeWitt <chessami92@gmail.com>
2014-01-28 17:41:55 -0600

    Factored out some common code into a function.

--- src/main/cnc.c
+++ src/main/cnc.c
@@ -11,6 +11,7 @@
 
 static void calculateMotorMovement( int32_t steps, MotorMovement_t *motorMovement,
 char fastestMotor, double *totalTime, double *constantSpeedTime );
+static void divideSteps( int32_t steps, int32_t totalAccelerationSteps, MotorMovement_t *motorMovement );
 static int32_t accelerationSteps( double acceleration, double speed );
 
 int sendBlock( Block *block ) {
@@ -34,22 +35,24 @@ char fastestMotor, double *totalTime, double *constantSpeedTime ) {
     totalAccelerationSteps = accelerationSteps( motorMovement->acceleration, motorMovement->speed );
 
     if( totalAccelerationSteps < steps ) {
-        motorMovement->accelerationSteps = ( totalAccelerationSteps + 0.5 ) / 2;
-        motorMovement->deaccelerationSteps = totalAccelerationSteps - motorMovement->accelerationSteps;
-        motorMovement->constantSpeedSteps = steps - totalAccelerationSteps;
+        divideSteps( steps, totalAccelerationSteps, motorMovement );
         if( fastestMotor ) {
             *constantSpeedTime = motorMovement->constantSpeedSteps / motorMovement->speed;
             *totalTime = 2 * motorMovement->speed / motorMovement->acceleration + *constantSpeedTime;
         }
     } else {
-        motorMovement->accelerationSteps = ( steps + 0.5 ) / 2;
-        motorMovement->deaccelerationSteps = steps - motorMovement->accelerationSteps;
-        motorMovement->constantSpeedSteps = 0;
+        divideSteps( steps, steps, motorMovement );
         if( fastestMotor ) {
             *constantSpeedTime = 0.0;
             *totalTime = 2.0 * sqrt( steps / motorMovement->acceleration );
         }
     }
+}
+
+static void divideSteps( int32_t steps, int32_t totalAccelerationSteps, MotorMovement_t *motorMovement ) {
+    motorMovement->accelerationSteps = ( totalAccelerationSteps + 0.5 ) / 2;
+    motorMovement->deaccelerationSteps = totalAccelerationSteps - motorMovement->accelerationSteps;
+    motorMovement->constantSpeedSteps = steps - totalAccelerationSteps;
 }
 
 static int32_t accelerationSteps( double acceleration, double speed ) {
