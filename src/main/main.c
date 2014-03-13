1a05c48793532c41a82891c27d22672cc6f24926
Josh DeWitt <jdewitt@unomaha.edu>
2014-03-12 23:09:01 -0500

    Now allow user to configure a read and write pipe.

--- src/main/main.c
+++ src/main/main.c
@@ -1,10 +1,19 @@
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "block.h"
 #include "cnc.h"
 #include "configure.h"
 
+#define openReadPipe() openPipe( readPipe, 0, O_RDONLY )
+#define openWritePipe() openPipe( writePipe, 1, O_WRONLY )
+
+static char *readPipe = NULL;
+static char *writePipe = NULL;
+
 static int startupAndConfigure( int argc, char *argv[] );
+static int openPipe( char *filename, int fdReplace, mode_t mode );
 
 int main( int argc, char *argv[] ) {
     char *line = NULL;
@@ -35,7 +44,7 @@ int main( int argc, char *argv[] ) {
 static int startupAndConfigure( int argc, char *argv[] ) {
     int i;
 
-    for( i = 1; i < argc; ++i ) {
+    for( i = 1; i < argc; i++ ) {
         if( argv[i][0] == '-' ) {
             switch( argv[i][1] ) {
                 case 'c':
@@ -43,12 +52,44 @@ static int startupAndConfigure( int argc, char *argv[] ) {
                         return 0;
                     }
                     break;
+                case 'r':
+                    readPipe = argv[i + 1];
+                    break;
+                case 'w':
+                    writePipe = argv[i + 1];
+                    break;
                 default:
                     fprintf( stderr, "ERROR: Unknown argument: %s\n", argv[i] );
                     return 0;
             }
         }
     }
+
+    return openReadPipe() && openWritePipe();
+}
+
+static int openPipe( char *filename, int fdReplace, mode_t mode ) {
+    int fd;
+
+    if( filename == NULL ) {
+        printf( "No pipe to open, using file descriptor %d as normal.\n", fdReplace );
+        return 1;
+    }
+
+    fd = open( filename, mode );
+
+    if( fd == -1 ) {
+        fprintf( stderr, "Could not open pipe at %s.\n", filename );
+        return 0;
+    }
+
+    printf( "Opened pipe at %s.\n", filename );
+
+    close( fdReplace );
+    dup( fd );
+    close( fd );
+
+    printf( "Duplicated pipe to file descriptor %d.\n", fdReplace );
 
     return 1;
 }
